7dfbba74741336d2805964ba1fd5f08e4f3b6f3d
Hennadii Borysevych <genuch3@gmail.com>
2023-02-27 15:03:32 +0200

    html and js added

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const URL =
+  'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cxrp%2Ccardano%2Cethereum%2Cdogecoin%2Ctether&vs_currencies=usd&include_24hr_change=true';
+fetch(URL)
+  .then(res => res.json())
+  .then(data => {
+    const container = document.querySelector('.container');
+    const coins = Object.getOwnPropertyNames(data);
+    for (const coin in coins) {
+      const coinInfo = data[`${coin}`];
+      const price = coinInfo.usd;
+      const change = coinInfo.usd_24h_change.toFixed(50);
+      container.innerHTML += `
+          <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+          <div class="coin__logo">
+          <img src="images/${coin}.png">
+          </div>
+          <div class="coin__name">
+          <h3>${coin}</h3>
+          <span>/USD</span>
+          </div>
+          <div class="coin__price">
+          <span class="price">$${price}<span>
+          <span class="change">$${change}<span>
+               </div>
+          </div>`;
+    }
+  });
